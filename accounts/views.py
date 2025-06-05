0e15713ef218191d9d53ef0967a5eef0b79cff70
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2025-06-05 13:40:59 +0545

    feat(registration): :sparkles: convert regiration to class based views

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.views import View
 from .forms import RegistrationForm
 
-def register(request):
-    if request.method == 'POST':
+
+class RegisterView(View):
+    def get(self, request):
+        form = RegistrationForm()
+        return render(request, "registration/register.html", {"form": form})
+
+    def post(self, request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # login(request, user) # Optional: Log the user in immediately after registration
-            return redirect('accounts:login') # Redirect to login page after successful registration
-    else:
-        form = RegistrationForm()
-    return render(request, 'registration/register.html', {'form': form})
+
+            # Optional: Log the user in immediately after registration
+            # login(request, user)
+
+            # Redirect to login page after successful registration
+            return redirect("accounts:login")
+        return render(request, "registration/register.html", {"form": form})
