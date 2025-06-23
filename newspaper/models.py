609bed5bc701dc04d6346602099d4b12d1beed76
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2025-06-23 13:27:50 +0545

    Bugfix

--- newspaper/models.py
+++ newspaper/models.py
@@ -70,10 +70,10 @@ class Advertisement(TimeStampModel):
 
 
 class Contact(TimeStampModel):
-    message = models.TextField()
     name = models.CharField(max_length=100)
     email = models.EmailField()
     subject = models.CharField(max_length=200)
+    message = models.TextField()
 
     def __str__(self):
         return self.name
