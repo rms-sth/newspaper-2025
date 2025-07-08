fe3cd4d34b90c424a1b23220d29f8ae7ef7825ef
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2025-07-08 10:29:40 +0545

    feat(dashboard): :sparkles: allow staff user to access dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,23 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, View, UpdateView
+from django.utils import timezone
+from django.views.generic import CreateView, ListView, UpdateView, View
 
 from dashboard.forms import CategoryForm, PostForm, TagForm
 from newspaper.models import Category, Post, Tag
 
-from django.utils import timezone
-from django.shortcuts import redirect
+
+class StaffRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_staff
+
+    def handle_no_permission(self):
+        return redirect("/")
 
 
 # Create your views here.
-class AdminPostListView(ListView):
+class AdminPostListView(StaffRequiredMixin, ListView):
     model = Post
     template_name = "dashboard/post_list.html"
     context_object_name = "posts"
@@ -18,7 +26,7 @@ class AdminPostListView(ListView):
         return Post.objects.all().order_by("-published_at")
 
 
-class AdminPostCreateView(CreateView):
+class AdminPostCreateView(StaffRequiredMixin, CreateView):
     model = Post
     template_name = "dashboard/post_create.html"
     form_class = PostForm
@@ -29,7 +37,7 @@ class AdminPostCreateView(CreateView):
         return super().form_valid(form)
 
 
-class AdminDraftPublishView(View):
+class AdminDraftPublishView(StaffRequiredMixin, View):
     def get(self, request, pk):
         post = Post.objects.get(pk=pk, published_at__isnull=True)
         post.published_at = timezone.now()
@@ -37,14 +45,14 @@ class AdminDraftPublishView(View):
         return redirect("admin-post-list")
 
 
-class AdminPostDeleteView(View):
+class AdminPostDeleteView(StaffRequiredMixin, View):
     def get(self, request, pk):
         post = Post.objects.get(pk=pk)
         post.delete()
         return redirect("admin-post-list")
 
 
-class AdminPostUpdateView(UpdateView):
+class AdminPostUpdateView(StaffRequiredMixin, UpdateView):
     model = Post
     template_name = "dashboard/post_create.html"
     form_class = PostForm
@@ -54,7 +62,7 @@ class AdminPostUpdateView(UpdateView):
 ######### Tag
 
 
-class AdminTagListView(ListView):
+class AdminTagListView(StaffRequiredMixin, ListView):
     model = Tag
     template_name = "dashboard/tag_list.html"
     context_object_name = "tags"
@@ -62,21 +70,22 @@ class AdminTagListView(ListView):
     def get_queryset(self):
         return Tag.objects.all()
 
-class AdminTagCreateView(CreateView):
+
+class AdminTagCreateView(StaffRequiredMixin, CreateView):
     model = Tag
     template_name = "dashboard/tag_create.html"
     form_class = TagForm
     success_url = reverse_lazy("admin-tag-list")
 
 
-class AdminTagDeleteView(View):
+class AdminTagDeleteView(StaffRequiredMixin, View):
     def get(self, request, pk):
         tag = Tag.objects.get(pk=pk)
         tag.delete()
         return redirect("admin-tag-list")
 
 
-class AdminTagUpdateView(UpdateView):
+class AdminTagUpdateView(StaffRequiredMixin, UpdateView):
     model = Tag
     template_name = "dashboard/tag_create.html"
     form_class = TagForm
@@ -86,7 +95,7 @@ class AdminTagUpdateView(UpdateView):
 ######### Category
 
 
-class AdminCategoryListView(ListView):
+class AdminCategoryListView(StaffRequiredMixin, ListView):
     model = Category
     template_name = "dashboard/category_list.html"
     context_object_name = "categories"
@@ -95,21 +104,21 @@ class AdminCategoryListView(ListView):
         return Category.objects.all()
 
 
-class AdminCategoryCreateView(CreateView):
+class AdminCategoryCreateView(StaffRequiredMixin, CreateView):
     model = Category
     template_name = "dashboard/category_create.html"
     form_class = CategoryForm
     success_url = reverse_lazy("admin-category-list")
 
 
-class AdminCategoryDeleteView(View):
+class AdminCategoryDeleteView(StaffRequiredMixin, View):
     def get(self, request, pk):
         category = Category.objects.get(pk=pk)
         category.delete()
         return redirect("admin-category-list")
 
 
-class AdminCategoryUpdateView(UpdateView):
+class AdminCategoryUpdateView(StaffRequiredMixin, UpdateView):
     model = Category
     template_name = "dashboard/category_create.html"
     form_class = CategoryForm
